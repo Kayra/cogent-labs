98a36e0e9d3507d774eb7f8c79af8e3d4b81fe19
Kayra <KayraAlat@gmail.com>
2021-04-01 12:34:58 +0100

    Added image upload and file persistence

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ Run server:
 ```bash
 uvicorn server:app --reload
 ```
+
+
+Upload image:
+
+```bash
+curl --request POST \
+--url http://127.0.0.1:8000/thumbnail/ \
+--header 'Content-Type: multipart/form-data' \
+--form image=@example_image.jpg
+```
--- server.py
+++ server.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+import shutil
+
+from fastapi import FastAPI, File, UploadFile
+
+from thumbnail_resizer import image_to_thumbnail
+
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 async def root():
-    return {"message": "Hello Cogent"}
+    return {'message': 'Hello Cogent'}
+
+
+@app.post('/thumbnail/')
+async def resize_image(image: UploadFile = File(...)):
+
+    with open('destination.jpg', 'wb') as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    return {'filename': image.filename}
