d7ec9f67debb03aed36f618de84af715eb346216
Kayra <KayraAlat@gmail.com>
2021-04-04 14:31:17 +0100

    Refactored fast views

--- server/app/main.py
+++ server/app/main.py
@@ -6,6 +6,7 @@ from celery.result import AsyncResult
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi import FastAPI, File, UploadFile, BackgroundTasks, Request
 
+from views.fast_views import router as views_router
 from validators import is_valid_image
 from worker import image_to_thumbnail
 
@@ -20,30 +21,7 @@ async def root():
     return {'message': 'Hello Cogent'}
 
 
-@app.post('/thumbnail/', status_code=202)
-async def resize_image(request: Request, background_tasks: BackgroundTasks, image: UploadFile = File(...)):
-
-    if not is_valid_image(image.file, image.filename, VALID_IMAGE_EXTENSIONS):
-        return JSONResponse(status_code=400,
-                            content={"Error": f"Image is not valid. Please ensure extension and file is of one of the following formats: {VALID_IMAGE_EXTENSIONS}"})
-
-    image_extension = os.path.splitext(image.filename)[1]
-    thumbnail_name = str(uuid.uuid4()) + image_extension
-
-    background_tasks.add_task(image_to_thumbnail, image.file, thumbnail_name)
-
-    thumbnail_url = os.path.join(str(request.url), thumbnail_name)
-
-    return {'Resized image URL': thumbnail_url}
-
-
-@app.get('/thumbnail/{thumbnail_name}', status_code=200)
-async def return_resized_image(thumbnail_name: str):
-
-    if os.path.isfile(thumbnail_name):
-        return FileResponse(thumbnail_name)
-    else:
-        return JSONResponse(status_code=404, content={"Error": "Image not found"})
+app.include_router(views_router)
 
 
 @app.post('/thumbnail-queue/', status_code=202)
