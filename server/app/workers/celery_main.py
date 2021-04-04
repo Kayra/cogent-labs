8594023689743004018f0332195a4d7f39323ff8
Kayra <KayraAlat@gmail.com>
2021-04-04 16:11:52 +0100

    Added config env var functionality to celery

--- server/app/workers/celery_main.py
+++ server/app/workers/celery_main.py
@@ -1,11 +1,13 @@
+import os
+
 from celery import Celery
 from celery.utils.log import get_task_logger
 
 
 celery_app = Celery(
     'tasks',
-    backend='redis://redis:6379/0',
-    broker='pyamqp://guest@rabbit//',
+    backend=os.getenv('CELERY_BACKEND', 'redis://redis:6379/0'),
+    broker=os.getenv('CELERY_BROKER', 'pyamqp://guest@rabbit//'),
     track_started=True
 )
 
